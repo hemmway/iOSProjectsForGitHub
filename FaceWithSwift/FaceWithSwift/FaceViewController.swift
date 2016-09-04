a35c0b40007dd13f4505d3b19e802730778f0ef8
Ivan <hemm.way@gmail.com>
2016-09-04 23:13:29 +0300

    New FaceWithSwift project
    I’m starting new project with Views, Interface Builder, Gestures and
    multiple MVCs

---
+++ FaceWithSwift/FaceWithSwift/FaceViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  FaceWithSwift
+//
+//  Created by Ivan Kurhanskyi on 9/4/16.
+//  Copyright © 2016 Ivan Kurhanskyi. All rights reserved.
+//
+
+import UIKit
+
+class FaceViewController: UIViewController {
+
+
+
+}
+
